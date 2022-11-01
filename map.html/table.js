c409639a1e24e8e4f6f489d972e8b218ace85fc1
Student <lab-209@aptech-msg.edu.pk>
2022-11-01 09:49:52 +0500

    this is over first commit

---
+++ map.html/table.js
@@ -0,0 +1,6 @@
+{"ID":1 ,"Name":"Bata" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"png"},
+{"ID":2 ,"Name":"Service" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"2.jfif"},
+{"ID":3 ,"Name":"Indure" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"3.jfif"},
+{"ID":4 ,"Name":"Jordan" ,"Category":"Shoes","Description":"This product is exclusively available in our website","img":"logo.png"}
+]
+        
